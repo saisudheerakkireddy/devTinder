249aaf162dad1395d897c7fe6d08b591d1057071
Sudheerakkireddy <sudheerakkireddy@gmail.com>
2024-12-02 00:20:15 +0530

    basic testing with postman

--- src/app.js
+++ src/app.js
@@ -6,12 +6,22 @@ app.listen(7777, ()=>{
     console.log("server is listening at port 7777")
 });
 
-app.use("/test",(req,res)=>{
-res.send("This is test")
-})
+// app.use("/test",(req,res)=>{
+// res.send("This is test")
+// })
 
 
 app.get("/test1",(req,res)=>{
-    res.send("This is req handler")
-})
+    res.send({firstname : "Sudheer"})
+});
 
+
+app.post("/user",(req,res)=>{
+
+    res.send("data is succesfully created")
+});
+
+app.delete("/user",(req,res) => {
+
+    res.send("deleted succesfully")
+});
