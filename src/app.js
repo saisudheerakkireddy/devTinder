e5a2888eaacd702420050ee15cd1f4956ba2fc38
Sudheerakkireddy <sudheerakkireddy@gmail.com>
2024-12-03 12:38:20 +0530

    dynamic- data in signup api

--- src/app.js
+++ src/app.js
@@ -6,23 +6,49 @@ const app = express();
 
 const UserModel = require("./models/user.js");
 
+app.use(express.json())
 
-app.post("/signup", async(req,res)=>{
-// creating a instance of the User model
 
-const user = new UserModel({
-    firstName: "Sudheer",
-    lastName: "Akkireddy",
-    emailId: "sudheerakkireddy@gmail.com",
-    password: "7780",
 
-});
+// app.post("/signup", async(req,res)=>{
+// // creating a instance of the User model
 
-await user.save();
+// const user = new UserModel({
+//     firstName: "Sudheer",
+//     lastName: "Akkireddy",
+//     emailId: "sudheerakkireddy@gmail.com",
+//     password: "7780",
 
-res.send("user added successfully")
+// });
 
-});
+// await user.save();
+
+// res.send("user added successfully")
+
+// });
+
+app.post("/signup", async (req,res)=> {
+
+    //creating a new instance of user model
+
+    const user = UserModel(req.body)
+
+    console.log(req.body)
+
+    //req.body converts the json object using middleware and save it to database ,, one important point is that it only stores the data 
+    //according to the defined user input schema
+
+   
+try{  
+   res.send("test")
+
+   await  user.save();}
+    catch (err){
+        res.status(400).send("something is wrong")
+    }
+ 
+
+})
 
 // app.post("/signup", (req,res)=> {
 //     const userObj = {
