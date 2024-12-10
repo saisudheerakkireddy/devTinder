8b68e0e45d424af33773b5dcc49d9b40cdb50a85
Sudheerakkireddy <sudheerakkireddy@gmail.com>
2024-12-10 18:40:09 +0530

    compound indexing

--- src/models/ConnectionRequest.js
+++ src/models/ConnectionRequest.js
@@ -28,6 +28,24 @@ const connectionRequestSchema = new mongoose.Schema( {
 },
 {
     timestamps:true,
+});
+
+// Compound index fromUserId to toUserId
+connectionRequestSchema.index({fromUserId : 1,toUserId : 1});
+
+
+
+
+connectionRequestSchema.pre("save",function () {
+
+    const connectionRequest =this;
+    // check if the from user id is same as touserId
+
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("Cannot send request to yourself");
+    }
+    next();
+
 })
 
 const ConnectionRequest = new mongoose.model("Connection Request", connectionRequestSchema);
